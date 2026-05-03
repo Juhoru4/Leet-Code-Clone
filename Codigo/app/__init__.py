f1b3c2fb25cb5676dc1b2f3edbaaa6785a0f81fe
vleria11 <vroblest@correo.usbcali.edu.co>
2026-05-03 17:55:32 -0500

    avance HU2 antes de actualizar main

--- Codigo/app/__init__.py
+++ Codigo/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .extensions import db
 import os
 from dotenv import load_dotenv
+from routes.problems import problems_bp
 
 load_dotenv()
 
@@ -11,10 +12,12 @@ def create_app():
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
         "SESSION_POOLER_URL",
-        os.getenv("DATABASE_URL")
+        os.getenv("DATABASE_URL", "sqlite:///test.db")
     )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
+
+    app.register_blueprint(problems_bp)
 
     return app
