dc246a18a5738f614a4a3c83167cd1fe66fee476
Darai <diramosz@correo.usbcali.edu.co>
2026-05-03 20:04:22 -0500

    Resuelve conflicto en __init__.py

--- Codigo/app/__init__.py
+++ Codigo/app/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from .extensions import db
 import os
 from dotenv import load_dotenv
-from routes.problems import problems_bp
-
 
 load_dotenv()
 
@@ -20,7 +18,7 @@ def create_app(database_uri=None):
 
     db.init_app(app)
 
-# Blueprints HU-03
+    # Blueprints HU-03
     from routes.ejecucion_endpoint import ejecucion_bp
     app.register_blueprint(ejecucion_bp)
 
@@ -35,17 +33,5 @@ def create_app(database_uri=None):
     from services.submissions_blueprint import submissions_bp
     app.register_blueprint(problems_bp, url_prefix="/api/problems")
     app.register_blueprint(submissions_bp, url_prefix="/api/submissions")
-    
-    
-    try:
-        from app.auth import bp as auth_bp
-        app.register_blueprint(auth_bp)
-    except Exception:
-        pass
 
-    from services.problemas_blueprint import problems_bp
-    from services.submissions_blueprint import submissions_bp
-    app.register_blueprint(problems_bp, url_prefix="/api/problems")
-    app.register_blueprint(submissions_bp, url_prefix="/api/submissions")
-    
     return app
